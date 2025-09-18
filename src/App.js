e8046f1c9d3f096e4c8fe21d8d8ed79517572645
Franco Zabala <asfalso@gmail.com>
2025-09-18 14:58:24 -0300

    feat: implement ticket #5 - add a button without implementation to login with google and meta

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from './LoginPage';
+import './App.css'; // Assuming an App.css might exist
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
